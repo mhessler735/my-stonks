fe0b99c4edd8b3ca0762ec013f1dcbb24053fe1f
Michael Hessler <mhessler735@gmail.com>
2025-10-01 23:39:45 -0500

    X code files

---
+++ MyStonk/MyStonk/ContentView.swift
@@ -0,0 +1,16 @@
+//
+//  ContentView.swift
+//  MyStonk
+//
+//  Created by Michael Hessler on 10/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello 👋")
+            .font(.largeTitle)
+            .padding()
+    }
+}
---
+++ MyStonk/MyStonk/MyStonkApp.swift
@@ -0,0 +1,17 @@
+‹›//
+//  MyStonkApp.swift
+//  MyStonk
+//
+//  Created by Michael Hessler on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct MyStonkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
